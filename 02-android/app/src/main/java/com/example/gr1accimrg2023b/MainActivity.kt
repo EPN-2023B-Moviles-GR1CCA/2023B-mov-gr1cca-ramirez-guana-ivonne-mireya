dbf88572876288d64cf8e5e994b8cd92969a5635
Ivonne-Ramirez <ivonne.ramirez@epn.edu.ec>
2023-12-14 16:49:47 -0500

    hasta ir actividad implementado

--- 02-android/app/src/main/java/com/example/gr1accimrg2023b/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/gr1accimrg2023b/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gr1accimrg2023b
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,5 +11,19 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+         botonCicloVida
+             .setOnClickListener{
+                 irActividad(ACicloVida::class.java)
+             }
     }
-}
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase )
+        startActivity(intent)
+        }
+    }
+
+
