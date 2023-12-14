0747e365a00fa63472b9a1b53cae0891cb040063
Ivonne-Ramirez <ivonne.ramirez@epn.edu.ec>
2023-12-14 16:31:06 -0500

    clase 06

--- 02-android/app/src/main/java/com/example/gr1accimrg2023b/ACicloVida.kt
+++ 02-android/app/src/main/java/com/example/gr1accimrg2023b/ACicloVida.kt
@@ -2,10 +2,55 @@ package com.example.gr1accimrg2023b
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.compose.material3.Snackbar
+import com.google.android.material.snackbar.Snackbar
 class ACicloVida : AppCompatActivity() {
+    var  textoGlobal=""
+    fun mostrarSnackbar(texto: String){
+        textoGlobal=textoGlobal+""+texto
+        com.google.android.material.snackbar.Snackbar
+            .make(
+                findViewById(R.id.cl_ciclo_vida),//view
+                textoGlobal, // texto
+                Snackbar.LENGTH_INDEFINITE //tiempo
+            )
+            .show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aciclo_vida)
+        mostrarSnackbar("OnCreate")
+    }
+override fun onStart(){
+    super.onStart()
+    mostrarSnackbar("onStart")
+
+}
+
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackbar("onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        mostrarSnackbar("onRestart")
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackbar("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackbar("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackbar("onDestroy")
     }
 }
