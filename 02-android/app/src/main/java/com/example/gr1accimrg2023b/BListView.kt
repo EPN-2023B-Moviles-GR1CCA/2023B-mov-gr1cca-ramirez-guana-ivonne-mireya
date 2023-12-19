87be4b17a1ae706f350601049e8830ae4b1a1698
Ivonne-Ramirez <ivonne.ramirez@epn.edu.ec>
2023-12-19 18:06:40 -0500

    clase 06

---
+++ 02-android/app/src/main/java/com/example/gr1accimrg2023b/BListView.kt
@@ -0,0 +1,11 @@
+package com.example.gr1accimrg2023b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_blist_view : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blist_view)
+    }
+}
