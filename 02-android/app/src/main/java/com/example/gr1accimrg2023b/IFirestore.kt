efa90404493d2089deacab78e9b9e47d3662cad6
Ivonne-Ramirez <ivonne.ramirez@epn.edu.ec>
2024-02-06 21:18:14 -0500

    clase y aplicacion

---
+++ 02-android/app/src/main/java/com/example/gr1accimrg2023b/IFirestore.kt
@@ -0,0 +1,11 @@
+package com.example.gr1accimrg2023b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2IFirestore : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity2_ifirestore)
+    }
+}
